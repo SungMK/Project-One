c91385dc2e8d0994f0a0264881672119b0f53971
SungMK <stwkim103@gmail.com>
2023-04-05 22:45:33 -0400

    Add initialize function

--- script.js
+++ script.js
@@ -17,24 +17,41 @@ let lives = 3;
 let score = 0;
 let firstCard = null;
 let secondCard = null;
-let guess;
+let message;
 
 // /*----- Cached Elements  -----*/
 const cardEls = document.querySelectorAll('.card');
 const displayLives = document.querySelector('#lives');
 const displayScore = document.querySelector('#score');
-const displayGuess = document.querySelector('#guess');
+const displayMessage = document.querySelector('#message');
 const playAgainBtn = document.querySelector('button');
 
 /*----- Event Listeners -----*/
+cardEls.forEach((card, cardIndexValue) => {
+  card.addEventListener('click', () => {
+  handleClick(card, cardIndexValue);
+  });
+})
+
 playAgainBtn.addEventListener('click', resetGame);
 
 /*----- Functions -----*/
-// initialize();
+initialize();
 
-// Need to create initialize function
-// Also need to break apart below code into smaller helper functions and organize. 
+function initialize() {
+  lives = 3;
+  score = 0;
+  firstCard = null;
+  secondCard = null;
 
+  revertToCardBackDefault();
+
+  cardEls.forEach((card, cardIndexValue) => {
+    card.addEventListener('click', handleCardClick(card, cardIndexValue));
+  });
+}
+
+// Need to break apart below code into smaller helper functions and clean up for better readability. 
 
 cardEls.forEach((card, cardIndexValue) => {
     card.addEventListener('click', () => {
@@ -50,7 +67,7 @@ cardEls.forEach((card, cardIndexValue) => {
           score+=1;
           firstCard = null;
           secondCard = null;
-          displayGuess.textContent = `${"It's a match!"}`;
+          displayMessage.textContent = `${"It's a match!"}`;
           displayScore.textContent = `Score: ${score}`;
         } else {
           lives-=1;
@@ -60,7 +77,7 @@ cardEls.forEach((card, cardIndexValue) => {
             firstCardEl.setAttribute('src', './Card Face Images/Back/blue2.svg');
             firstCard = null;
             secondCard = null;
-            displayGuess.textContent = `${'Wrong Guess! Try again!'}`;
+            displayMessage.textContent = `${'Wrong Guess! Try again!'}`;
             displayLives.textContent = `Lives: ${lives}`;
           }, 1500);
         }
@@ -70,7 +87,7 @@ cardEls.forEach((card, cardIndexValue) => {
 
 function playerWins() {
   if (score === 5) {
-    displayGuess.textContent = `${'Congratulations! You win!'}`;
+    displayMessage.textContent = `${'Congratulations! You win!'}`;
     setTimeout(() => {
       resetGame();
     }, 3000);
@@ -79,11 +96,17 @@ function playerWins() {
 
 function playerLoses() {
   if (lives === 0) {
-    displayGuess.textContent = `${'You have no more lives! You lose!'}`;
+    displayMessage.textContent = `${'You have no more lives! You lose!'}`;
     setTimeout(() => {
       resetGame();
     }, 3000);
   }
+}
+
+function revertToCardBackDefault() {
+  cardEls.forEach(card => {
+    card.setAttribute('src', './Card Face Images/Back/blue2.svg');
+  });
 }
 
 function resetGame() {
@@ -91,13 +114,10 @@ function resetGame() {
   score = 0;
   firstCard = null;
   secondCard = null;
-  cardEls.forEach(card => {
-    card.setAttribute('src', './Card Face Images/Back/blue2.svg');
-    displayLives.textContent = `${'Lives: 3'}`;
-    displayScore.textContent = `${'Score: 0'}`;
-    displayGuess.textContent = `${'Click any card to begin!'}`;
-  });
+
+  revertToCardBackDefault();
+
+  displayLives.textContent = `${'Lives: 3'}`;
+  displayScore.textContent = `${'Score: 0'}`;
+  displayMessage.textContent = `${'Click any card to begin!'}`;
 }
-
-
-
