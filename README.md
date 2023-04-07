b0387e6e8609349a0f04cbaa31b65759e3a143bd
SungMK <stwkim103@gmail.com>
2023-04-07 02:20:46 -0400

    Update README and edit simplify comments

--- README.md
+++ README.md
@@ -1,18 +1,46 @@
-# Concentration Game
+# About
 
-## What is concentration?
+## What is Concentration?
 Concentration is a memory game where a number of cards are arranged in a grid, face down. Each card has a picture or symbol on it, and the goal is to match pairs until there are no more cards left. While typically played using playing cards, it can also be played using a specific theme. The game can be played individually or against multiple players. 
 
-### PseudoCode
-- The page will load, and the game will automatically initialize.
-- The player will click on a card, flipping the card around to reveal its value (e.g. Ace, 2, 3, 4, etc). 
-- The player will then choose a second card, flipping and revealing its value.
-- If the values match, then both cards will remain faced up, and the "Guess" section will update to "Correct!" and the player can continue to match cards.
-- If the values do not match, the "Guess" section will update to "Wrong Guess! Try again!". 
-    - There will be a brief delay to allow players to take note of the values using setTimeout().
-    - Then, the cards will return to the "face down" state, allowing the player to attempt to match a pair again.
-    - In addition to the message being displayed, the player will lose a life from the desiginated "Lives" section. 
-        - If and when a player has lost all their lives (i.e., lives reaches 0), a message will appear stating "You lost!", and a "Play Again!" button will appear.
-            - If and when the player clicks this button, the game will reset, the cards will shuffle, and the player can start again with the maximum number of lives again.
-- Once a player has correctly identified every matching pair, a message will appear stating, "Congratulations! You win!", and a "Play Again!" button will appear.
-    - If and when the player clicks this button, the game will reset, the cards will shuffle, and the player can start again with the maximum number of lives again.
+<br>
+
+<img src="./README Images/Game Start.png" width=80%>
+
+<br>
+
+## Built With:
+- HTML
+- CSS
+- JavaScript
+
+<br>
+
+# Getting Started
+Click the link below and start the game by clicking on any card!
+
+
+
+<br>
+
+# Gameplay 
+
+## Match cards to earn points!
+
+<br>
+
+<img src="./README Images/Correct Guess.png" width=80%>
+
+<br>
+
+## Don't lose lives!
+<br>
+
+<img src="./README Images/Wrong Guess.png" width=80%>
+
+<br>
+
+# Icebox items
+- Feature 1: Shuffle function
+- Feature 2: Option to choose different levels of difficulty with varying number of lives and gameboard sizes
+- Feature 3: Player versus AI option
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*----- Constants -----*/
-const cardFronts = [ // Creates an array of objects with each object representing a card front face
+const cardFronts = [ // Array of objects with each object representing a card front face
   {img: './Card Face Images/Fronts/hearts_2.svg'}, // Idx 0
   {img: './Card Face Images/Fronts/hearts_2.svg'}, // Idx 1
   {img: './Card Face Images/Fronts/diamonds_4.svg'}, // Idx 2
@@ -20,37 +20,37 @@ let secondCard = null;
 let message;
 
 // /*----- Cached Elements  -----*/
-const cardEls = document.querySelectorAll('.card'); // Selects all elements with the 'card' class and stores them in a variable called cardEls
-const displayLives = document.querySelector('#lives'); // Selects the HTML 'lives' id and stores it in a variable called displayLives
-const displayScore = document.querySelector('#score'); // Selects the HTML 'score' id and stores it in a variable called displayScore
-const displayMessage = document.querySelector('#message'); // Selects the HTML 'message' id and stores it in a variable called displayMessage
-const playAgainBtn = document.querySelector('button'); // Selects the HTML btn element and assigns it to a variable called playAgainBtn
+const cardEls = document.querySelectorAll('.card'); 
+const displayLives = document.querySelector('#lives'); 
+const displayScore = document.querySelector('#score'); 
+const displayMessage = document.querySelector('#message'); 
+const playAgainBtn = document.querySelector('button'); 
 
 /*----- Event Listeners -----*/
 cardEls.forEach((card, cardIndexValue) => { 
   card.addEventListener('click', handleCardClick(card, cardIndexValue));
 });
 
-playAgainBtn.addEventListener('click', initialize); // This adds an event listener to the "Play Again" button so that when a 'click' is registered, it calls the initialize function, which resets the game.
+playAgainBtn.addEventListener('click', initialize); 
 
 /*----- Functions -----*/
-initialize(); // When the initialize function is invoked, it starts the game. 
+initialize(); // Game starts automatically 
 
-function initialize() { // This function sets the values at the start of the game and also doubles as a reset function for when the game is reset automatically or when the player clicks the "Play Again" button.
+function initialize() { // Sets the default values at the start of the game and also doubles as a reset function.
   lives = 3;
   score = 0;
   firstCard = null;
   secondCard = null;
 
-  resetCardImages(); // This function resets the img srcs for every card
+  resetCardImages(); 
 
-  displayLives.textContent = 'Lives: 3'; // Rather than hard coding the HTML with the strings of text, it will be updated when the game initializes
+  displayLives.textContent = 'Lives: 3'; 
   displayScore.textContent = 'Score: 0'; 
   displayMessage.textContent = 'Click any card to begin!';
 }
 
 function handleCardClick(card, cardIndexValue) { // Callback function for Event Listener that takes 'card' and 'cardIndexValue' as parameters.
-  return () => { // Returns anonymous function
+  return () => { 
     const clickedCard = cardFronts[cardIndexValue]; // Variable created to store img src of card that player clicked
     card.setAttribute('src', clickedCard.img); // Clicked card is updated with the img of the corresponding index of the array
       
@@ -69,18 +69,18 @@ function handleCardClick(card, cardIndexValue) { // Callback function for Event 
 }
 
 function handleCardMatch() {
-  score+=1; // Score is incremented by 1
-  firstCard = null; // firstCard variable is reset to null so player can select new pair of cards
-  secondCard = null; // secondCard variable is also reset to null so player can select new pair of cards
-  displayMessage.textContent = "It's a match!"; // The 'Message' section will update to "It's a match!"
+  score+=1; 
+  firstCard = null; // firstCard variable is reset to null so player can select new card
+  secondCard = null; // secondCard variable is also reset to null so player can select new card
+  displayMessage.textContent = "It's a match!"; // The 'Message' section will update
   displayScore.textContent = `Score: ${score}`; // Score section will update to display the current score
   
-  playerWins(); // Function is invoked and checks if the score is set to 5. If it's not equal to 5, nothing happens and the game continues; but, if the score is equal to 5, the function is executed
+  playerWins(); // Function invoked and checks if win condition met; game continues if not met
 }
 
 function handleWrongMatch(card) {
-  lives -=1; // Score is decremented by 1
-  displayMessage.textContent = 'Wrong Guess! Try again!'; // The 'Message section will update to 'Wrong Guess! Try again!'
+  lives -=1; 
+  displayMessage.textContent = 'Wrong Guess! Try again!'; // The 'Message section will update
   displayLives.textContent = `Lives: ${lives}`; // Score section will update to display current number of lives
   setTimeout(() => { 
     card.setAttribute('src', './Card Face Images/Back/blue2.svg'); // Sets the clicked card's img back to the default
@@ -89,30 +89,30 @@ function handleWrongMatch(card) {
     firstCard = null; // Resets value of firstCard to null (i.e. no card picked)
     secondCard = null; // Resets value of secondCard to null (i.e. no card picked)
 
-    playerLoses(); 
+    playerLoses(); // Function invoked and checks if lose condition met; game continues if not met
   }, 1500);
 }
 
 function playerWins() {
-  if (score === 5) { // Checks if score is equal to 5
-    displayMessage.textContent = 'Congratulations! You win!!!'; // If score is equal to 5, the 'Message' section will update to 'Congratulations! You win!'
+  if (score === 5) { 
+    displayMessage.textContent = 'Congratulations! You win!!!'; // If score is equal to 5, the 'Message' section will update
     setTimeout(() => { // After displaying the message, setTimeout will run intialize after 3 seconds
-      initialize(); // Initialize function invoked
+      initialize(); 
     }, 3000);
   }
 }
 
 function playerLoses() { 
-  if (lives === 0) { // Checks if lives are equal to 0
-    displayMessage.textContent = 'You have no more lives! You lose!'; // If lives are equal to 0, the 'Message' section will update to 'You have no more lives! You lose!'
+  if (lives === 0) { 
+    displayMessage.textContent = 'You have no more lives! You lose!'; // If lives are equal to 0, the 'Message' section will update
     setTimeout(() => { // After displaying the message, setTimeout will run initialize after 3 seconds
-      initialize(); // Initialize function invoked
+      initialize(); 
     }, 3000);
   }
 }
 
-function resetCardImages() { // This function resets the card images back to the default back face
-  cardEls.forEach(card => { // Loops through each card and updates every img src to default img using setAttribute
+function resetCardImages() { // Resets card images by looping through each card and updating each imc src to default using setAttribute
+  cardEls.forEach(card => { 
     card.setAttribute('src', './Card Face Images/Back/blue2.svg');  
   });
 }
